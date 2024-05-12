4df0a02cea1fa3c894d52925158a60074891de16
Никита Спирин <gromilla03@gmail.com>
2024-05-13 02:12:29 +0300

    Добавьте файлы проекта.

---
+++ app/forms.py
@@ -0,0 +1,47 @@
+﻿"""
+Definition of forms.
+"""
+from django.db import models
+from .models import Comment
+from .models import Blog
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import gettext_lazy as _
+
+class BootstrapAuthenticationForm(AuthenticationForm):
+    """Authentication form which uses boostrap CSS."""
+    username = forms.CharField(max_length=254,
+                               widget=forms.TextInput({
+                                   'class': 'form-control',
+                                   'placeholder': 'Логин'}))
+    password = forms.CharField(label=_("Password"),
+                               widget=forms.PasswordInput({
+                                   'class': 'form-control',
+                                   'placeholder':'Пароль'}))
+    
+class AnketaForm(forms.Form):
+    name = forms.CharField(label='Ваше имя', min_length=2, max_length=100)
+    age_choices = [(i, str(i)) for i in range(18, 101)]  # Создаем список возможных значений от 18 до 100 лет
+    age = forms.ChoiceField(choices=age_choices, label='Выберите ваш возраст')
+    gender = forms.ChoiceField(label='Ваш пол', choices=[('1', 'Мужской'),('2', 'Женский')], widget=forms.RadioSelect, initial=1)
+    city = forms.CharField(label='Ваш город', min_length=2, max_length=100)
+    vopros = forms.ChoiceField(label='Как часто вы посещаете различные культурные мероприятия?', 
+                               choices=(('1', 'Не посещаю вообще'), ('2', '1-2 раза в год'), ('3', 'Раз в пару месяцев'), ('4', 'Раз в месяц'), ('5', 'Больше одного раза в месяц')))
+    message = forms.CharField(label='Ваши пожелания по сайту', widget=forms.Textarea(attrs={'rows':12, 'cols':20}))
+    notice = forms.BooleanField(label='Получать рассылку о новых мероприятиях на почту?', required=False)
+    email = forms.EmailField(label='Ваш E-Mail', min_length=7)
+    
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment # используемая модель
+        fields = ('text',) # требуется заполнить только поле text
+        labels = {'text': "Комментарий"} # метка к полю формы text
+# author будет автоматически выбран в зависимости от авторизованного пользователя
+# date автоматически добавляется в момент создания записи
+        
+class BlogForm(forms.ModelForm):
+    class Meta:
+        model = Blog # используемая модель
+        fields = ('title', 'description', 'content', 'image',) # заполняемые поля
+        labels = {'title': "Заголовок", 'description': "Краткое содержание", 'content': "Полное содержание", 'image': "Картинка"}
---
+++ app/views.py
@@ -0,0 +1,213 @@
+﻿"""
+Definition of views.
+"""
+
+from datetime import date, datetime
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from .forms import AnketaForm 
+from django.contrib.auth.forms import UserCreationForm
+
+from django.db import models
+from .models import Blog
+
+from .models import Comment # использование модели комментариев
+from .forms import CommentForm # использование формы ввода комментария
+from .forms import BlogForm # использование формы ввода статьи блога
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Домашняя страница',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Контакты',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'О нас',
+            'year':datetime.now().year,
+        }
+    )
+
+def links(request):
+    """Рендерит страницу с полезными ресурсами."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/links.html',
+        {
+            'title':'Полезные ресурсы',
+            'year':datetime.now().year,
+        }
+    )
+
+def pool(request):
+    """Рендерит страницу обратной связи."""
+    assert isinstance(request, HttpRequest)
+    data = None
+    gender = {'1': 'Мужчина', '2': 'Женщина'}
+    vopros = {'1': 'Не посещаю вообще', '2': '1-2 раза в год', '3': 'Раз в пару месяцев', '4': 'Раз в месяц', '5': 'Больше одного раза в месяц'}
+    if request.method == 'POST':
+        form = AnketaForm(request.POST)
+        if form.is_valid():
+            data = dict()
+            data['name'] = form.cleaned_data['name']
+            data['age'] = form.cleaned_data['age']
+            data['gender'] = form.cleaned_data['gender']
+            data['city'] = form.cleaned_data['city']
+            data['vopros'] = form.cleaned_data['vopros']
+            data['message'] = form.cleaned_data['message']
+            if(form.cleaned_data['notice'] == True):
+                data['notice'] = 'Да'
+            else:
+                data['notice'] = 'Нет'
+            data['email'] = form.cleaned_data['email']
+            form = None
+    else:
+        form = AnketaForm()
+    return render(
+        request,
+        'app/pool.html',
+        {
+            'form':form,
+            'data':data,
+            'title':'Обратная связь',
+            'year':datetime.now().year,
+        }
+    )
+
+def registration(request):
+    """Renders the registration page."""
+    if request.method == "POST": # после отправки формы
+        regform = UserCreationForm(request.POST)
+        if regform.is_valid():
+            reg_f = regform.save(commit=False)
+            reg_f.is_staff = False # запрещён вход в административный раздел
+            reg_f.is_active = True # активный пользователь
+            reg_f.is_superuser = False # не является суперпользователем
+            reg_f.date_joined = datetime.now() # дата регистрации
+            reg_f.last_login = datetime.now()  # дата последней авторизации
+
+            regform.save() # сохраняем изменения после добавления полей
+
+            return redirect('home') # переадресация на главную страницу после регистрации
+    else:
+        regform = UserCreationForm()  # создание объекта формы для ввода данных
+
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/registration.html',
+        {
+            'regform': regform, # передача формы в шаблон веб-страницы
+
+            "year" :datetime.now().year,
+        }
+    )
+
+def blog(request):
+    """Renders the blog page."""
+    posts = Blog.objects.all() # запрос на выбор всех статей блога из модели
+
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/blog.html',
+        {
+        'title':'Блог',
+        'posts': posts, # передача списка статей в шаблон веб-страницы
+        'year' :datetime.now().year
+        }
+    )
+
+def blogpost(request, parametr):
+    """Renders the blogpost page."""
+    assert isinstance(request, HttpRequest)
+    post_1 = Blog.objects.get(id=parametr) # запрос на выбор конкретной статьи по параметру
+    comments = Comment.objects.filter(post=parametr)
+    
+    if request.method == "POST": # после отправки данных формы на сервер методом POST
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment_f = form.save(commit=False)
+            comment_f.author = request.user
+            comment_f.date = datetime.now()
+            comment_f.post = Blog.objects.get(id=parametr)
+            comment_f.save() # сохраняем изменения после добавления полей
+
+            return redirect('blogpost', parametr=post_1.id)  # переадресация на ту же страницу статьи после отправки комментария
+    else:
+        form = CommentForm() # создание формы для ввода комментария
+
+    return render(
+        request,
+        'app/blogpost.html',
+        {
+            'post_1': post_1, # передача конкретной статьи в шаблон веб-страницы
+            'comments': comments, # передача всех комментариев к данной статье в шаблон веб-страницы
+            'form': form, # передача формы добавления комментария в шаблон веб-страницы
+            'year':datetime.now().year,
+        }
+    )
+
+def newpost(request):
+    """Renders the newpost page."""
+    assert isinstance(request, HttpRequest)
+
+    if request.method == "POST": # после отправки формы
+        blogform = BlogForm(request.POST, request.FILES)
+        if blogform.is_valid():
+            blog_f = blogform.save(commit=False)
+            blog_f.posted = datetime.now()
+            blog_f.autor = request.user
+
+            blog_f.save() # сохраняем изменения после добавления полей
+            return redirect('blog') # переадресация на страницу Блог после создания статьи Блога
+    else:
+        blogform = BlogForm() # создание объекта формы для ввода данных
+
+    return render(
+        request,
+        'app/newpost.html',
+        {
+
+            'blogform': blogform, # передача формы в шаблон веб-страницы
+            'title': 'Добавить статью блога',
+
+            'year' :datetime.now().year,
+        }
+    )
+
+def videopost(request):
+    """Renders the videopost page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/videopost.html',
+        {
+            'title':'Видео',
+            'year':datetime.now().year,
+        }
+    )
